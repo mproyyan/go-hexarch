89d7769f98e922e843ecd523ca28dc98813a9874
royyan <mproyyan@gmail.com>
2022-08-31 09:27:49 +0700

    create gin server

---
+++ internal/adapters/servers/gin/gin.go
@@ -0,0 +1,34 @@
+package ginserver
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type GinServer struct {
+	*gin.Engine
+	port         string
+	repositories map[string]interface{}
+	services     map[string]interface{}
+	handlers     map[string]interface{}
+}
+
+func NewGinServer(port string) *GinServer {
+	gs := &GinServer{
+		Engine:       gin.Default(),
+		port:         port,
+		repositories: make(map[string]interface{}),
+		services:     make(map[string]interface{}),
+		handlers:     make(map[string]interface{}),
+	}
+
+	return gs
+}
+
+func (gs *GinServer) Run() {
+	log.Fatal(
+		gs.Engine.Run(fmt.Sprintf(":%s", gs.port)),
+	)
+}
