f54a3ed1e92158abb8c5376cb727255e4d92bee2
mproyyan <mproyyan@gmail.com>
2022-12-13 18:30:19 +0700

    generate ProductRepository mock with new updated interface

--- mocks/ProductRepository.go
+++ mocks/ProductRepository.go
@@ -5,7 +5,9 @@ package mocks
 import (
 	context "context"
 
+	databases "github.com/mproyyan/gin-rest-api/internal/adapters/databases"
 	domain "github.com/mproyyan/gin-rest-api/internal/application/domain"
+
 	mock "github.com/stretchr/testify/mock"
 )
 
@@ -14,13 +16,13 @@ type ProductRepository struct {
 	mock.Mock
 }
 
-// Delete provides a mock function with given fields: ctx, productId
-func (_m *ProductRepository) Delete(ctx context.Context, productId int) error {
-	ret := _m.Called(ctx, productId)
+// Delete provides a mock function with given fields: ctx, dbtx, productId
+func (_m *ProductRepository) Delete(ctx context.Context, dbtx databases.DBTX, productId int) error {
+	ret := _m.Called(ctx, dbtx, productId)
 
 	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, int) error); ok {
-		r0 = rf(ctx, productId)
+	if rf, ok := ret.Get(0).(func(context.Context, databases.DBTX, int) error); ok {
+		r0 = rf(ctx, dbtx, productId)
 	} else {
 		r0 = ret.Error(0)
 	}
@@ -28,13 +30,13 @@ func (_m *ProductRepository) Delete(ctx context.Context, productId int) error {
 	return r0
 }
 
-// Find provides a mock function with given fields: ctx, productId
-func (_m *ProductRepository) Find(ctx context.Context, productId int) (*domain.Product, error) {
-	ret := _m.Called(ctx, productId)
+// Find provides a mock function with given fields: ctx, dbtx, productId
+func (_m *ProductRepository) Find(ctx context.Context, dbtx databases.DBTX, productId int) (*domain.Product, error) {
+	ret := _m.Called(ctx, dbtx, productId)
 
 	var r0 *domain.Product
-	if rf, ok := ret.Get(0).(func(context.Context, int) *domain.Product); ok {
-		r0 = rf(ctx, productId)
+	if rf, ok := ret.Get(0).(func(context.Context, databases.DBTX, int) *domain.Product); ok {
+		r0 = rf(ctx, dbtx, productId)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*domain.Product)
@@ -42,8 +44,8 @@ func (_m *ProductRepository) Find(ctx context.Context, productId int) (*domain.P
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context, int) error); ok {
-		r1 = rf(ctx, productId)
+	if rf, ok := ret.Get(1).(func(context.Context, databases.DBTX, int) error); ok {
+		r1 = rf(ctx, dbtx, productId)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -51,13 +53,13 @@ func (_m *ProductRepository) Find(ctx context.Context, productId int) (*domain.P
 	return r0, r1
 }
 
-// FindAll provides a mock function with given fields: ctx
-func (_m *ProductRepository) FindAll(ctx context.Context) ([]*domain.Product, error) {
-	ret := _m.Called(ctx)
+// FindAll provides a mock function with given fields: ctx, dbtx
+func (_m *ProductRepository) FindAll(ctx context.Context, dbtx databases.DBTX) ([]*domain.Product, error) {
+	ret := _m.Called(ctx, dbtx)
 
 	var r0 []*domain.Product
-	if rf, ok := ret.Get(0).(func(context.Context) []*domain.Product); ok {
-		r0 = rf(ctx)
+	if rf, ok := ret.Get(0).(func(context.Context, databases.DBTX) []*domain.Product); ok {
+		r0 = rf(ctx, dbtx)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).([]*domain.Product)
@@ -65,8 +67,8 @@ func (_m *ProductRepository) FindAll(ctx context.Context) ([]*domain.Product, er
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
-		r1 = rf(ctx)
+	if rf, ok := ret.Get(1).(func(context.Context, databases.DBTX) error); ok {
+		r1 = rf(ctx, dbtx)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -74,13 +76,13 @@ func (_m *ProductRepository) FindAll(ctx context.Context) ([]*domain.Product, er
 	return r0, r1
 }
 
-// Save provides a mock function with given fields: ctx, product
-func (_m *ProductRepository) Save(ctx context.Context, product domain.Product) (*domain.Product, error) {
-	ret := _m.Called(ctx, product)
+// Save provides a mock function with given fields: ctx, dbtx, product
+func (_m *ProductRepository) Save(ctx context.Context, dbtx databases.DBTX, product domain.Product) (*domain.Product, error) {
+	ret := _m.Called(ctx, dbtx, product)
 
 	var r0 *domain.Product
-	if rf, ok := ret.Get(0).(func(context.Context, domain.Product) *domain.Product); ok {
-		r0 = rf(ctx, product)
+	if rf, ok := ret.Get(0).(func(context.Context, databases.DBTX, domain.Product) *domain.Product); ok {
+		r0 = rf(ctx, dbtx, product)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*domain.Product)
@@ -88,8 +90,8 @@ func (_m *ProductRepository) Save(ctx context.Context, product domain.Product) (
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context, domain.Product) error); ok {
-		r1 = rf(ctx, product)
+	if rf, ok := ret.Get(1).(func(context.Context, databases.DBTX, domain.Product) error); ok {
+		r1 = rf(ctx, dbtx, product)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -97,13 +99,13 @@ func (_m *ProductRepository) Save(ctx context.Context, product domain.Product) (
 	return r0, r1
 }
 
-// Update provides a mock function with given fields: ctx, product
-func (_m *ProductRepository) Update(ctx context.Context, product domain.Product) (*domain.Product, error) {
-	ret := _m.Called(ctx, product)
+// Update provides a mock function with given fields: ctx, dbtx, product
+func (_m *ProductRepository) Update(ctx context.Context, dbtx databases.DBTX, product domain.Product) (*domain.Product, error) {
+	ret := _m.Called(ctx, dbtx, product)
 
 	var r0 *domain.Product
-	if rf, ok := ret.Get(0).(func(context.Context, domain.Product) *domain.Product); ok {
-		r0 = rf(ctx, product)
+	if rf, ok := ret.Get(0).(func(context.Context, databases.DBTX, domain.Product) *domain.Product); ok {
+		r0 = rf(ctx, dbtx, product)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*domain.Product)
@@ -111,8 +113,8 @@ func (_m *ProductRepository) Update(ctx context.Context, product domain.Product)
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context, domain.Product) error); ok {
-		r1 = rf(ctx, product)
+	if rf, ok := ret.Get(1).(func(context.Context, databases.DBTX, domain.Product) error); ok {
+		r1 = rf(ctx, dbtx, product)
 	} else {
 		r1 = ret.Error(1)
 	}
